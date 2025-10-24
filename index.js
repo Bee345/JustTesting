9e74d8633b24331509a5ae66272d6ae71e7bde9e
Bee345 <lilbeezee8@gmail.com>
2025-10-24 14:37:20 +0100

    I Have added the JavaScript Functionality to it but i need it to have transition when it's coming

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // This Is where i have started My JS Settings For My landing Page....
-let navBtn = document.getElementById("navBtn");
+let navBtn = document.querySelector(".btnCont");
+let navBar = document.getElementById("navBar");
+
+navBtn.addEventListener("click", () => { 
+    navBar.classList.toggle("show");
+});
